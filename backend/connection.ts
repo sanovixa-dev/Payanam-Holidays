232349c05c5403cb47fe19c351fc2934ef2fd828
sanovixa-dev <sanovixa@gmail.com>
2026-08-18 18:18:56 +0530

    change connection pool

--- backend/connection.ts
+++ backend/connection.ts
@@ -2,6 +2,12 @@ import "dotenv/config";
 import { Pool } from "pg";
 const connectionString = process.env.DATABASE_URL;
 
-const pool = new Pool({ connectionString: connectionString });
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl: {
+    // Required for web-hosted production databases
+    rejectUnauthorized: false,
+  },
+});
 
 export default pool;
