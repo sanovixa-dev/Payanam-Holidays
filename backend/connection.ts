dd7504ebbd809b6298c56eaded560aa0df6b08f7
sanovixa-dev <sanovixa@gmail.com>
2026-08-18 19:39:08 +0530

    DATABASE_URL to DATABASE_PUBLIC_URL

--- backend/connection.ts
+++ backend/connection.ts
@@ -1,7 +1,8 @@
 import "dotenv/config"; // Must stay at the absolute top of the page!
 import pg from "pg";
 
-const connectionString = process.env.DATABASE_PUBLIC_URL;
+const connectionString =
+  process.env.DATABASE_PUBLIC_URL || process.env.DATABASE_URL;
 
 // Add this quick console debug line to prove what your server sees at launch
 console.log(
